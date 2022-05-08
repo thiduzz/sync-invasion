1b2cf0479017918587ab63df8dbc4d9e92f6b209
Thiago <thiduzz14@gmail.com>
2022-05-08 13:44:49 +0200

    Extend Location struct

--- internal/nodes/location.go
+++ internal/nodes/location.go
@@ -1,3 +1,26 @@
 package nodes
 
-type Location struct{}
+import "github.com/thiduzz/code-kata-invasion/internal/constant"
+
+type Location struct {
+	Id    uint
+	Name  string
+	State map[constant.LocationState]bool
+}
+
+func NewLocation(id uint, name string) Location {
+	return Location{Id: id, Name: name}
+}
+
+type LocationInterface interface {
+	GetId() uint
+	GetName() string
+}
+
+func (l Location) GetId() uint {
+	return l.Id
+}
+
+func (l Location) GetName() string {
+	return l.Name
+}
