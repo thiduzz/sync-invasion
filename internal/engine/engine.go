aa8ffaf144bd867855e9385353f743667105d204
Thiago <thiduzz14@gmail.com>
2022-05-08 17:34:48 +0200

    Scaffold engine - which will be running the main operations

---
+++ internal/engine/engine.go
@@ -0,0 +1,17 @@
+package engine
+
+import "github.com/thiduzz/code-kata-invasion/internal/nodes"
+
+type Engine struct {
+	Locations    *nodes.LocationCollection
+	AttackersQty uint
+	MaxMoves     uint
+}
+
+func NewEngine(locations *nodes.LocationCollection, attackersQty uint, maxMoves uint) *Engine {
+	return &Engine{Locations: locations, AttackersQty: attackersQty, MaxMoves: maxMoves}
+}
+
+func (en *Engine) Start() error {
+	return nil
+}
