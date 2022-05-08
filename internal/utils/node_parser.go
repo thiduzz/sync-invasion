0cf1b009831cb1bba4d41938eadfe1c9a3fbb2f0
Thiago <thiduzz14@gmail.com>
2022-05-08 15:30:50 +0200

    Create new location and add to the collection

--- internal/utils/node_parser.go
+++ internal/utils/node_parser.go
@@ -9,17 +9,21 @@ import (
 	"strings"
 )
 
+//ParseNodes Utility function - For reading the map file and convert it into a collection of locations
+// Assumption: even for the links specified within the file are considered, meaning, if a city A points to city B,
+// i won't be creating a link from city B to city A, only a link from city A to city B
 func ParseNodes(filePath *string) (*nodes.LocationCollection, error) {
 	if *filePath == "" {
 		return nil, errors.NewCommandError(constant.MapFilePathParameter, "missing parameter")
 	}
+
 	file, err := os.Open(*filePath)
 	if err != nil {
 		return nil, err
 	}
-
+	//guarantees the file will be closed when function is finished/application crashes
 	defer file.Close()
-	cities := nodes.NewLocationCollection()
+	locations := nodes.NewLocationCollection()
 	scanner := bufio.NewScanner(file)
 	id := uint(1)
 	for scanner.Scan() {
@@ -30,8 +34,9 @@ func ParseNodes(filePath *string) (*nodes.LocationCollection, error) {
 			return nil, errors.NewUtilsError("ParseMap", "invalid row in parsed file")
 		}
 
-		cityName := pieces[0]
-		cities.Add(nodes.NewLocation(id, cityName, pieces[1:]))
+		locationName := pieces[0]
+		location := nodes.NewLocation(id, locationName)
+		locations.Add(location)
 		id++
 	}
 
@@ -39,5 +44,5 @@ func ParseNodes(filePath *string) (*nodes.LocationCollection, error) {
 		return nil, errors.NewUtilsErrorWrap("ParseMap", err)
 	}
 
-	return cities, nil
+	return locations, nil
 }
