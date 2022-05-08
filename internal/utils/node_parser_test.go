f1ca2809eb544019175cb5080221c35797e6dddf
Thiago <thiduzz14@gmail.com>
2022-05-08 13:06:59 +0200

    Add read file success

--- internal/utils/node_parser_test.go
+++ internal/utils/node_parser_test.go
@@ -32,6 +32,14 @@ func TestParseNodes(t *testing.T) {
 			want:    nil,
 			wantErr: true,
 		},
+		{
+			name: "should be able to read existing file",
+			args: args{
+				filePath: ptrStr("../../resources/world-map.txt"),
+			},
+			want:    nil,
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
