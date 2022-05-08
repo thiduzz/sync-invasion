a9073e7104e36e3ac25c420334210ad461217641
Thiago <thiduzz14@gmail.com>
2022-05-08 22:18:46 +0200

    Add basic tests for attack

--- internal/engine/engine_test.go
+++ internal/engine/engine_test.go
@@ -3,8 +3,11 @@ package engine
 import (
 	"errors"
 	"github.com/golang/mock/gomock"
+	"github.com/thiduzz/code-kata-invasion/internal/constant"
 	mock_nodes "github.com/thiduzz/code-kata-invasion/internal/mock/nodes"
 	"github.com/thiduzz/code-kata-invasion/internal/nodes"
+	"math/rand"
+	"reflect"
 	"testing"
 )
 
@@ -90,6 +93,106 @@ func TestEngine_PrepareAttackers(t *testing.T) {
 			}
 			if err := en.PrepareAttackers(tt.args.factoryMockFunc(ctrl)); (err != nil) != tt.wantErr {
 				t.Errorf("PrepareAttackers() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestEngine_attack(t *testing.T) {
+	type fields struct {
+		Locations      *nodes.LocationCollection
+		randomizerFunc func() *rand.Rand
+	}
+	type args struct {
+		attacker *nodes.Attacker
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *nodes.Location
+		wantErr bool
+	}{
+		{
+			name: "should error when retrieved attacker is dead",
+			fields: fields{
+				Locations: nil,
+				randomizerFunc: func() *rand.Rand {
+					source := rand.NewSource(10)
+					return rand.New(source)
+				},
+			},
+			args: args{
+				attacker: &nodes.Attacker{
+					Id:   1,
+					Name: "Test",
+					State: map[constant.AttackerState]bool{
+						constant.Dead: true,
+					},
+					Location: nil,
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "should error when retrieved attacker is trapped",
+			fields: fields{
+				Locations: nil,
+				randomizerFunc: func() *rand.Rand {
+					source := rand.NewSource(10)
+					return rand.New(source)
+				},
+			},
+			args: args{
+				attacker: &nodes.Attacker{
+					Id:   1,
+					Name: "Test",
+					State: map[constant.AttackerState]bool{
+						constant.Trapped: true,
+					},
+					Location: nil,
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "should return random new location when attacker has none (on attacker first iteration)",
+			fields: fields{
+				Locations: &nodes.LocationCollection{
+					Collection:   nil,
+					ReferenceMap: nil,
+				},
+				randomizerFunc: func() *rand.Rand {
+					source := rand.NewSource(10)
+					return rand.New(source)
+				},
+			},
+			args: args{
+				attacker: &nodes.Attacker{
+					Id:       1,
+					Name:     "Test",
+					Location: nil,
+				},
+			},
+			want:    &nodes.Location{},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			en := &Engine{
+				Locations:  tt.fields.Locations,
+				Randomizer: tt.fields.randomizerFunc(),
+			}
+			got, err := en.attack(tt.args.attacker)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("attack() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("attack() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
